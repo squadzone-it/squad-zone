0acd9e15c041d7d9792a20efae9007f11949f9ab
guzmangriancho <guzmangonzalez2001@gmail.com>
2023-04-10 18:39:09 +0200

    user data print

--- src/screens/ProfileScreen.js
+++ src/screens/ProfileScreen.js
@@ -9,13 +9,18 @@ import {
 import Ionic from "react-native-vector-icons/Ionicons";
 import BackgroundTabs from "../components/BackgroundTabs";
 import { theme } from "../core/theme";
-import React from 'react';
+import React, { useState, useEffect } from "react";
 import Button from "../components/Button";
-import  Paragraph  from "../components/Paragraph";
+import Paragraph from "../components/Paragraph";
+import Logo from "../components/Logo";
 
-
+import { useNavigation } from "@react-navigation/native";
 
 const settingsButton = () => {};
+
+const handlePress = async () => {
+	const data = await getUserData();
+};
 
 const Header = () => {
 	return (
@@ -27,10 +32,7 @@ const Header = () => {
 				SQUAD Z<Ionic name="football-outline" style={{ fontSize: 23 }} />
 				NE
 			</Text>
-			<TouchableOpacity
-				onPress={settingsButton}
-				style={styles.headerButtonRight}
-			>
+			<TouchableOpacity onPress={handlePress} style={styles.headerButtonRight}>
 				<Ionic
 					name="options"
 					style={{ fontSize: 25, color: theme.colors.text }}
@@ -40,8 +42,7 @@ const Header = () => {
 	);
 };
 
-const ProfileInfo = () => {
-	
+const ProfileInfo = ({ data }) => {
 	return (
 		<View
 			style={{
@@ -74,7 +75,7 @@ const ProfileInfo = () => {
 						color: theme.colors.text,
 					}}
 				>
-					@username
+					@{data.nombre_usuario}
 				</Text>
 				<Text
 					style={{
@@ -85,69 +86,84 @@ const ProfileInfo = () => {
 						color: theme.colors.text,
 					}}
 				>
-					Guzmán G. Riancho
+					{data.nombre} {data.apellidos}
 				</Text>
 			</View>
 		</View>
-		
 	);
 };
 
-const Body = () => {
+const Body = ({ data }) => {
+	const navigation = useNavigation();
+
+	const onLogoutPressed = () => {
+		navigation.navigate("StartScreen");
+	};
 	return (
-        <View style={{ backgroundColor: theme.colors.surface, height: "100%" }}>
-            <StatusBar
-                backgroundColor={theme.colors.surface}
-                barStyle="light-content"
-                animated={true}
-            />
-            <ProfileInfo />
-            <Button mode="contained" onPress={getUserData}>
-                LOGOUT
-            </Button>
-			<Paragraph>
-				
-			</Paragraph>
-        </View>
-    );
+		<View style={{ backgroundColor: theme.colors.surface, height: "100%" }}>
+			<StatusBar
+				backgroundColor={theme.colors.surface}
+				barStyle="light-content"
+				animated={true}
+			/>
+			<ProfileInfo data={data} />
+			<Button mode="contained" onPress={onLogoutPressed}>
+				LOGOUT
+			</Button>
+			<Paragraph></Paragraph>
+		</View>
+	);
 };
 
 const ProfileScreen = () => {
+	const [data, setData] = useState(null);
+
+	useEffect(() => {
+		async function fetchData() {
+			const result = await getUserData();
+			setData(result);
+		}
+		fetchData();
+	}, []);
+
 	return (
 		<BackgroundTabs>
 			<Header />
-			<Body />
+			{data ? <Body data={data} /> : <Logo />}
 		</BackgroundTabs>
 	);
 };
 
+async function getUserData() {
+	try {
+		const response = await fetch(
+			"https://readdatauser-zvcc2bcxkq-nw.a.run.app",
+			{
+				method: "POST",
+				headers: {
+					"Content-Type": "application/json",
+				},
+				body: JSON.stringify({
+					id: "rDhKT0cFAlOgZvXdEX9CFriwnpp2",
+				}),
+			}
+		);
 
-
-  async function getUserData() {
-    try {
-      const response = await fetch('https://readdatauser-zvcc2bcxkq-nw.a.run.app', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({
-          id: 'rDhKT0cFAlOgZvXdEX9CFriwnpp2',
-        }),
-      });
-
-      if (response.ok) {
-        const data = await response.json();
-        console.log('Datos del usuario obtenidos correctamente:', data);
-		return data
-        //setUser(data);
-      } else {
-        console.error('Error al obtener los datos del usuario:', response.statusText);
-      }
-    } catch (error)
-	{
-		console.error('Error al obtener los datos del usuario:', error);
-	  }
+		if (response.ok) {
+			const data = await response.json();
+			console.log("Datos del usuario obtenidos correctamente:", data);
+			return data;
+			//setUser(data);
+		} else {
+			console.error(
+				"Error al obtener los datos del usuario:",
+				response.statusText
+			);
+		}
+	} catch (error) {
+		console.error("Error al obtener los datos del usuario:", error);
 	}
+}
 
 export default ProfileScreen;
 
