93b87e53856025036959f93a873ccc43c2e29185
guzmangriancho <guzmangonzalez2001@gmail.com>
2023-06-25 00:28:03 +0200

    firebase functions

---
+++ functions/index.js
@@ -0,0 +1,63 @@
+// The Cloud Functions for Firebase SDK to create Cloud Functions and set up triggers.
+const functions = require("firebase-functions");
+
+// The Firebase Admin SDK to access Firestore.
+const admin = require("firebase-admin");
+admin.initializeApp();
+const db = admin.firestore();
+
+exports.saveUserData = functions
+	.region("europe-west2")
+	.https.onRequest(async (req, res) => {
+		if (req.method !== "POST") {
+			res.status(400).send("Invalid request method. Please use POST.");
+			return;
+		}
+
+		const { userId, name, lastName, email, username } = req.body;
+
+		try {
+			await db.collection("users").doc(userId).set({
+				name,
+				lastName,
+				email,
+				username,
+			});
+			console.log("User data saved to Firestore successfully:", userId);
+			res.status(200).send({ result: "success" });
+		} catch (error) {
+			console.error("Error saving user data to Firestore:", error);
+			res.status(500).send({ result: "error", error: error.message });
+		}
+	});
+
+exports.getUserData = functions
+	.region("europe-west2")
+	.https.onRequest(async (req, res) => {
+		console.log("Request received:", req.body); // Agrega esta línea
+
+		const { userId } = req.body;
+		if (!userId) {
+			res.status(400).send("Error: El userId es requerido");
+			return;
+		}
+
+		try {
+			const db = admin.firestore();
+			const userRef = db.collection("users").doc(userId);
+			const userDoc = await userRef.get();
+
+			if (!userDoc.exists) {
+				res.status(404).send("Error: Usuario no encontrado");
+				return;
+			}
+
+			console.log("User data found:", userDoc.data()); // Agrega esta línea
+			res.status(200).json({ result: "success", data: userDoc.data() }); // Agrega el objeto "result"
+		} catch (error) {
+			console.error(error);
+			res
+				.status(500)
+				.send("Error: No se pudo obtener la información del usuario");
+		}
+	});
