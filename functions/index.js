86f1c2ecb106152c012601aca35143927eaa4ba3
Luis <luis.collado.pu@gmail.com>
2023-06-28 16:41:22 +0200

    Apis y apis

--- functions/index.js
+++ functions/index.js
@@ -230,52 +230,75 @@ exports.searchUsers = functions
             return;
         }
 
-        const { squadId, name, captain, members, userId } = req.body;
+        const { name, captain } = req.body; // Recibimos el nombre del equipo y el capitán
 
         try {
+            const userRef = db.collection("users").doc(captain);
+            const user = await userRef.get();
+            
+            // Comprobamos si el usuario ya tiene un equipo
+            if (user.exists && user.data().team) {
+                res.status(400).send({ result: "error", error: "User already in a team" });
+                return;
+            }
+            
+            // Creamos un nuevo equipo
             const squadData = {
                 name,
-                captain,  // YOUR_CAPTAIN_DATA
-                members,  // YOUR_MEMBERS_DATA
+                captain,
+                members: [captain], // El capitán es el primer miembro
+                invitations: [], // Inicializamos las invitaciones vacías
+                requests: [] // Inicializamos las solicitudes vacías
             };
+            const squadRef = db.collection("squads").doc();
+            await squadRef.set(squadData);
 
-            await db.collection("squads").doc(squadId).set(squadData);
-
-            console.log("Squad data saved to Firestore successfully:", squadId);
-
-            // Ahora actualizamos el campo 'squad' en el documento de usuario
-            await db.collection("users").doc(userId).update({
-                squad: name
+            // Actualizamos el documento del usuario
+            await userRef.update({
+                team: squadRef.id
+                //squadInvitations: admin.firestore.FieldValue.arrayUnion(squadRef.id)
             });
-
-            console.log("User data updated with squad name successfully:", userId);
 
             res.status(200).send({ result: "success" });
         } catch (error) {
-            console.error("Error saving squad data to Firestore or updating user data:", error);
+            console.error("Error creating squad or updating user data:", error);
             res.status(500).send({ result: "error", error: error.message });
         }
     });
+
 
 	exports.deleteSquad = functions
 	.region("europe-west2")
 	.https.onRequest(async (req, res) => {
 		if (req.method !== "DELETE") {
-			res.status(400).send("Invalid request method. Please use POST.");
+			res.status(400).send("Invalid request method. Please use DELETE.");
 			return;
 		}
-
+	
 		const { userId, squadId } = req.body;
-
+	
 		try {
+			// Obtenemos la Squad
+			const squadRef = db.collection("squads").doc(squadId);
+			const squadDoc = await squadRef.get();
+			const squadData = squadDoc.data();
+	
+			// Verificamos que el equipo solo tenga un miembro
+			if (squadData.members.length > 1) {
+				res.status(400).send({ result: "error", error: "You must expel all members before deleting the squad." });
+				return;
+			}
+	
 			// Eliminar la Squad
-			await db.collection("squads").doc(squadId).delete();
-			
+			await squadRef.delete();
+	
 			// Vacia el campo squad del usuario
 			await db.collection("users").doc(userId).update({
-				squad: admin.firestore.FieldValue.delete()
+				team: admin.firestore.FieldValue.delete()
+				//squadInvitations: admin.firestore.FieldValue.arrayRemove(squadId),
+				//squadRequests: admin.firestore.FieldValue.arrayRemove(squadId)
 			});
-
+	
 			console.log(`Squad ${squadId} deleted successfully and user ${userId} squad field cleared.`);
 			res.status(200).send({ result: "success" });
 		} catch (error) {
@@ -284,4 +307,360 @@ exports.searchUsers = functions
 		}
 	});
 
+	exports.inviteToSquad = functions
+.region("europe-west2")
+.https.onRequest(async (req, res) => {
+	if (req.method !== "POST") {
+		res.status(400).send("Invalid request method. Please use POST.");
+		return;
+	}
 
+	const { squadId, captainId, userId } = req.body;
+
+	try {
+		const squadRef = db.collection("squads").doc(squadId);
+		const userRef = db.collection("users").doc(userId);
+
+		// Verificar que el capitán es el que hace la invitación
+		const squadDoc = await squadRef.get();
+		if (!squadDoc.exists || squadDoc.data().captain !== captainId) {
+			res.status(400).send({ result: "error", error: "You must be the captain to invite users." });
+			return;
+		}
+
+		// Verificar que el usuario no pertenece a un equipo
+		const userDoc = await userRef.get();
+		if (userDoc.exists && userDoc.data().team) {
+			res.status(400).send({ result: "error", error: "The user is already in a team." });
+			return;
+		}
+
+		// Verificar que no exista ya una invitación
+		if (squadDoc.data().invitations.includes(userId)) {
+			res.status(400).send({ result: "error", error: "The user has already been invited." });
+			return;
+		}
+
+		// Agregar la invitación
+		await squadRef.update({
+			invitations: admin.firestore.FieldValue.arrayUnion(userId)
+		});
+
+		await userRef.update({
+			squadInvitations: admin.firestore.FieldValue.arrayUnion(squadId)
+		});
+
+		res.status(200).send({ result: "success" });
+	} catch (error) {
+		console.error("Error inviting user to squad:", error);
+		res.status(500).send({ result: "error", error: error.message });
+	}
+});
+
+	
+exports.handleInvitation = functions
+.region("europe-west2")
+.https.onRequest(async (req, res) => {
+	if (req.method !== "PUT") {
+		res.status(400).send("Invalid request method. Please use POST.");
+		return;
+	}
+
+	const { squadId, userId, accept } = req.body;
+
+	try {
+		const squadRef = db.collection("squads").doc(squadId);
+		const userRef = db.collection("users").doc(userId);
+
+		// Verificar que el usuario fue invitado
+		/*const squadDoc = await squadRef.get();
+		if (!squadDoc.exists || !squadDoc.data().invitations.includes(userId)) {
+			res.status(400).send({ result: "error", error: "The user was not invited to this squad." });
+			return;
+		}
+		*/
+
+		// Remover la invitación sin importar si el usuario acepta o rechaza
+		await squadRef.update({
+			invitations: admin.firestore.FieldValue.arrayRemove(userId)
+		});
+
+		await userRef.update({
+			squadInvitations: admin.firestore.FieldValue.arrayRemove(squadId)
+		});
+
+		// Si el usuario acepta la invitación
+		if (accept) {
+			await squadRef.update({
+				members: admin.firestore.FieldValue.arrayUnion(userId)
+			});
+
+			await userRef.update({
+				team: squadId
+			});
+		}
+
+		res.status(200).send({ result: "success" });
+	} catch (error) {
+		console.error("Error handling squad invitation:", error);
+		res.status(500).send({ result: "error", error: error.message });
+	}
+});
+
+exports.requestToJoinSquad = functions
+.region("europe-west2")
+.https.onRequest(async (req, res) => {
+    if (req.method !== "POST") {
+        res.status(400).send("Invalid request method. Please use POST.");
+        return;
+    }
+
+    const { squadId, userId } = req.body;
+
+    try {
+        const squadRef = db.collection("squads").doc(squadId);
+        const userRef = db.collection("users").doc(userId);
+
+        const userDoc = await userRef.get();
+        const squadDoc = await squadRef.get();
+
+        if (!userDoc.exists || !squadDoc.exists) {
+            res.status(404).send({ result: "error", error: "User or Squad does not exist." });
+            return;
+        }
+
+		if (userDoc.data().team || (userDoc.data().squadRequests && userDoc.data().squadRequests.includes(squadId))) {
+			res.status(400).send({ result: "error", error: "User already has a team or has already requested to join this squad." });
+			return;
+		}
+		
+        // Añade la solicitud en el equipo
+        await squadRef.update({
+            requests: admin.firestore.FieldValue.arrayUnion(userId)
+        });
+
+        // Añade la solicitud en el usuario
+        await userRef.update({
+            squadRequests: admin.firestore.FieldValue.arrayUnion(squadId)
+        });
+
+        res.status(200).send({ result: "success" });
+    } catch (error) {
+        console.error("Error creating join request:", error);
+        res.status(500).send({ result: "error", error: error.message });
+    }
+});
+
+exports.handleRequest = functions
+.region("europe-west2")
+.https.onRequest(async (req, res) => {
+    if (req.method !== "PUT") {
+        res.status(400).send("Invalid request method. Please use PUT.");
+        return;
+    }
+
+    const { squadId, userId, accept } = req.body;
+
+    try {
+        const squadRef = db.collection("squads").doc(squadId);
+        const userRef = db.collection("users").doc(userId);
+
+        const userDoc = await userRef.get();
+        const squadDoc = await squadRef.get();
+
+        if (!squadDoc.data().requests.includes(userId) && userDoc.data().squadRequests.includes(squadId)) {
+            res.status(400).send({ result: "error", error: "No request from user to join this squad." });
+            return;
+        }
+
+        // Aceptamos la solicitud
+        if (accept) {
+            await squadRef.update({
+                members: admin.firestore.FieldValue.arrayUnion(userId),
+                requests: admin.firestore.FieldValue.arrayRemove(userId)
+            });
+
+            await userRef.update({
+                team: squadId,
+                squadRequests: admin.firestore.FieldValue.arrayRemove(squadId)
+            });
+        }
+        // Rechazamos la solicitud
+        else {
+            await squadRef.update({
+                requests: admin.firestore.FieldValue.arrayRemove(userId)
+            });
+
+            await userRef.update({
+                squadRequests: admin.firestore.FieldValue.arrayRemove(squadId)
+            });
+        }
+
+        res.status(200).send({ result: "success" });
+    } catch (error) {
+        console.error("Error handling request:", error);
+        res.status(500).send({ result: "error", error: error.message });
+    }
+});
+
+exports.leaveOrKickSquad = functions
+    .region("europe-west2")
+    .https.onRequest(async (req, res) => {
+        if (req.method !== "POST") {
+            res.status(400).send("Invalid request method. Please use POST.");
+            return;
+        }
+
+        const { userId, squadId } = req.body;
+
+        try {
+            const squadRef = db.collection("squads").doc(squadId);
+            const squadDoc = await squadRef.get();
+
+            if (!squadDoc.exists) {
+                res.status(404).send({ result: "error", error: "Squad does not exist." });
+                return;
+            }
+
+            const members = squadDoc.data().members;
+            const captain = squadDoc.data().captain;
+
+            // Si el usuario no es miembro del equipo
+            if (!members.includes(userId)) {
+                res.status(400).send({ result: "error", error: "User is not a member of the squad." });
+                return;
+            }
+
+            // Si el usuario es el capitán
+            if (captain === userId) {
+                // Si es el último miembro, elimina el equipo
+                if (members.length === 1) {
+                    await squadRef.delete();
+                    console.log(`Squad ${squadId} deleted as last member (the captain) left.`);
+                } else {
+                    // Si no, haz capitán al siguiente miembro
+                    const nextCaptain = members.find(member => member !== userId);
+                    await squadRef.update({
+                        captain: nextCaptain,
+                        members: admin.firestore.FieldValue.arrayRemove(userId)
+                    });
+                    console.log(`User ${userId} left the squad ${squadId}, and user ${nextCaptain} is now the captain.`);
+                }
+            } else {
+                // Si el usuario no es el capitán, simplemente lo eliminamos del equipo
+                await squadRef.update({
+                    members: admin.firestore.FieldValue.arrayRemove(userId)
+                });
+                console.log(`User ${userId} has been kicked out of squad ${squadId}.`);
+            }
+
+            // En todos los casos, actualizamos el documento del usuario para remover el equipo
+            const userRef = db.collection("users").doc(userId);
+            await userRef.update({
+                team: admin.firestore.FieldValue.delete(),
+                squadInvitations: admin.firestore.FieldValue.arrayRemove(squadId),
+                squadRequests: admin.firestore.FieldValue.arrayRemove(squadId)
+            });
+            console.log(`User ${userId} has left the squad ${squadId}.`);
+
+            res.status(200).send({ result: "success" });
+        } catch (error) {
+            console.error("Error leaving or kicking squad:", error);
+            res.status(500).send({ result: "error", error: error.message });
+        }
+    });
+
+	exports.editSquad = functions
+    .region("europe-west2")
+    .https.onRequest(async (req, res) => {
+        if (req.method !== "POST") {
+            res.status(400).send("Invalid request method. Please use POST.");
+            return;
+        }
+
+        const { squadId, userId, clubBadgeUrl, description, location } = req.body;
+
+        try {
+            const squadRef = db.collection("squads").doc(squadId);
+            const squadDoc = await squadRef.get();
+
+            if (!squadDoc.exists) {
+                res.status(404).send({ result: "error", error: "Squad does not exist." });
+                return;
+            }
+
+            // Comprobar que el usuario es el capitán del equipo
+            const captain = squadDoc.data().captain;
+            if (captain !== userId) {
+                res.status(403).send({ result: "error", error: "Only the captain can edit the squad." });
+                return;
+            }
+
+            // Actualizar el documento del equipo
+            await squadRef.update({
+                clubBadgeUrl,
+                description,
+                location
+            });
+
+            console.log(`Squad ${squadId} edited successfully by captain ${userId}.`);
+            res.status(200).send({ result: "success" });
+        } catch (error) {
+            console.error("Error editing squad:", error);
+            res.status(500).send({ result: "error", error: error.message });
+        }
+    });
+
+	exports.changeRole = functions
+    .region("europe-west2")
+    .https.onRequest(async (req, res) => {
+        if (req.method !== "PUT") {
+            res.status(400).send("Invalid request method. Please use PUT.");
+            return;
+        }
+
+        const { squadId, captainId, memberId, isVeteran } = req.body;
+
+        try {
+            const squadRef = db.collection("squads").doc(squadId);
+            const squadDoc = await squadRef.get();
+
+            if (!squadDoc.exists) {
+                res.status(404).send({ result: "error", error: "Squad does not exist." });
+                return;
+            }
+
+            // Comprobar que el usuario es el capitán del equipo
+            const captain = squadDoc.data().captain;
+            if (captain == captainId) {
+                res.status(403).send({ result: "error "+captain, error: "Only the captain can change roles." });
+                return;
+            }
+/*
+            // Comprobar que el miembro está en el equipo
+            const members = squadDoc.data().members;
+            if (!members.includes(memberId)) {
+                res.status(404).send({ result: "error", error: "Member not in squad." });
+                return;
+            }
+			*/
+
+            // Cambiar el rol del miembro
+            if (isVeteran) {
+                await squadRef.update({
+                    veterans: admin.firestore.FieldValue.arrayUnion(memberId)
+                });
+            } else {
+                await squadRef.update({
+                    veterans: admin.firestore.FieldValue.arrayRemove(memberId)
+                });
+            }
+
+            const newRole = isVeteran ? "Veteran" : "Member";
+            console.log(`Role of member ${memberId} changed to ${newRole} by captain ${captainId} in squad ${squadId}.`);
+            res.status(200).send({ result: "success" });
+        } catch (error) {
+            console.error("Error changing role:", error);
+            res.status(500).send({ result: "error", error: error.message });
+        }
+    });
