47b657315eccacad8bf61701f17ee61db4a3aa1f
Luis <luis.collado.pu@gmail.com>
2023-07-11 12:28:56 +0200

    2

--- src/screens/MatchDetailsScreen.js
+++ src/screens/MatchDetailsScreen.js
@@ -1,13 +1,16 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { View, Text, StyleSheet, Image, TouchableOpacity } from "react-native";
 import Ionic from "react-native-vector-icons/Ionicons";
 import { theme } from "../core/theme";
 import BackgroundTabs from "../components/BackgroundTabs";
+import {getMatchData } from "../components/ApiService";
 
 const MatchDetailsScreen = ({ navigation, route }) => {
 	// Extract the 'match' object from the route parameters
 	const { match } = route.params;
+	const [partidoData, setPartidoData] = useState(null);
 
+     /*
 	const partidoData = {
 		escudo1:
 			"https://firebasestorage.googleapis.com/v0/b/squadzoneapp.appspot.com/o/squad_prS6rA4BO9CpjvC4jfx9_20230704T190025669Z.png?alt=media&token=2bcf69f5-9373-44d1-8973-0f97d9e07cb7",
@@ -21,126 +24,143 @@ const MatchDetailsScreen = ({ navigation, route }) => {
 		ubicacion: "Mortera",
 		jugadores1: ["Jugador 1", "Jugador 2", "Jugador 3"],
 		jugadores2: ["Jugador 1", "Jugador 2", "Jugador 3"],
-	};
+	};*/
+	useEffect(() => {
+		// Supongamos que 'matchId' es una prop pasada a este componente
+		const fetchMatchData = async () => {
+			try {
+				const data = await getMatchData(match.matchId);
+				console.log(data);  
+				setPartidoData(data);
+			} catch (error) {
+				console.error('Error:', error);
+			}
+		};
+		fetchMatchData();
+	}, []);
+	
 
 	const tempButton = () => {};
 
 	return (
-		<BackgroundTabs>
+		partidoData ? (
+		  <BackgroundTabs>
 			{/*   Header   */}
 			<View style={styles.header}>
-				<TouchableOpacity
-					style={styles.headerButtonLeft}
-					onPress={() => navigation.goBack()}
-				>
-					<Ionic
-						name="arrow-back"
-						style={{ fontSize: 32, color: theme.colors.text }}
-					/>
-				</TouchableOpacity>
-				<Text style={styles.headerText}>Pickup 4v4</Text>
-				<TouchableOpacity onPress={tempButton} style={styles.headerButtonRight}>
-					<Ionic
-						name="chatbubble-ellipses-outline"
-						style={{ fontSize: 25, color: theme.colors.text }}
-					/>
-				</TouchableOpacity>
+			  <TouchableOpacity
+				style={styles.headerButtonLeft}
+				onPress={() => navigation.goBack()}
+			  >
+				<Ionic
+				  name="arrow-back"
+				  style={{ fontSize: 32, color: theme.colors.text }}
+				/>
+			  </TouchableOpacity>
+			  <Text style={styles.headerText}>Pickup 4v4</Text>
+			  <TouchableOpacity onPress={tempButton} style={styles.headerButtonRight}>
+				<Ionic
+				  name="chatbubble-ellipses-outline"
+				  style={{ fontSize: 25, color: theme.colors.text }}
+				/>
+			  </TouchableOpacity>
 			</View>
 			{/*   Match info header   */}
 			<View style={styles.container}>
-				<View style={styles.matchHeader}>
-					<View style={styles.team}>
-						<Image
-							source={{ uri: partidoData.escudo1 }}
-							style={styles.shield}
-						/>
-						<Text style={styles.teamName}>{partidoData.nombre1}</Text>
-					</View>
-					<View style={styles.score}>
-						<Text style={styles.scoreText}>{partidoData.marcador}</Text>
-						<Text style={styles.statusText}>{partidoData.status}</Text>
-					</View>
-					<View style={styles.team}>
-						<Image
-							source={{ uri: partidoData.escudo2 }}
-							style={styles.shield}
-						/>
-						<Text style={styles.teamName}>{partidoData.nombre2}</Text>
-					</View>
+			  <View style={styles.matchHeader}>
+				<View style={styles.team}>
+				  <Image
+					source={{ uri: partidoData.team1.logo }}
+					style={styles.shield}
+				  />
+				  <Text style={styles.teamName}>{partidoData.team1.name}</Text>
 				</View>
-				<Text style={styles.location}>{partidoData.ubicacion}</Text>
-				<Text style={styles.dateTime}>{partidoData.fechaHora}</Text>
-				<View style={styles.playerListContainer}>
-					<View style={styles.playerList}>
-						<Text
-							style={{
-								fontFamily: "SF-Pro-Semibold",
-								fontSize: 14,
-								paddingBottom: 10,
-								alignSelf: "center",
-							}}
-						>
-							{partidoData.nombre1}
-						</Text>
-						{partidoData.jugadores1.map((jugador, index) => (
-							<View style={styles.userContainer} key={index}>
-								<Image
-									source={{
-										uri: "https://firebasestorage.googleapis.com/v0/b/squadzoneapp.appspot.com/o/defaultPP.png?alt=media&token=7f90b50b-6321-484a-9d14-295fbfcfc32f",
-									}}
-									style={styles.userImageL}
-								/>
-								<View style={styles.userSubContainer}>
-									<View style={styles.usernameContainerL}>
-										<Text style={styles.usernameText}>{jugador}</Text>
-										<Text style={styles.nameText}>{jugador}</Text>
-									</View>
-								</View>
-							</View>
-						))}
-					</View>
-					<View style={styles.playerList && { alignItems: "flex-end" }}>
-						<Text
-							style={{
-								fontFamily: "SF-Pro-Semibold",
-								fontSize: 14,
-								paddingBottom: 10,
-								alignSelf: "center",
-							}}
-						>
-							{partidoData.nombre2}
-						</Text>
-						{partidoData.jugadores2.map((jugador, index) => (
-							<View style={styles.userContainer} key={index}>
-								<View style={styles.userSubContainer}>
-									<View style={styles.usernameContainerR}>
-										<Text style={styles.usernameText}>{jugador}</Text>
-										<Text style={styles.nameText}>{jugador}</Text>
-									</View>
-								</View>
-								<Image
-									source={{
-										uri: "https://firebasestorage.googleapis.com/v0/b/squadzoneapp.appspot.com/o/defaultPP.png?alt=media&token=7f90b50b-6321-484a-9d14-295fbfcfc32f",
-									}}
-									style={styles.userImageR}
-								/>
-							</View>
-						))}
-					</View>
+				<View style={styles.score}>
+				  <Text style={styles.scoreText}>{`${partidoData.score.team1} - ${partidoData.score.team2}`}</Text>
+				  <Text style={styles.statusText}>{partidoData.status}</Text>
 				</View>
+				<View style={styles.team}>
+				  <Image
+					source={{ uri: partidoData.team2.logo }}
+					style={styles.shield}
+				  />
+				  <Text style={styles.teamName}>{partidoData.team2.name}</Text>
+				</View>
+			  </View>
+			  <Text style={styles.location}>{partidoData.location}</Text>
+			  <Text style={styles.dateTime}>{partidoData.date}</Text>
+			  <View style={styles.playerListContainer}>
+				<View style={styles.playerList}>
+				  <Text
+					style={{
+					  fontFamily: "SF-Pro-Semibold",
+					  fontSize: 14,
+					  paddingBottom: 10,
+					  alignSelf: "center",
+					}}
+				  >
+					{partidoData.team1.name}
+				  </Text>
+				  {partidoData.team1.players.map((player, index) => (
+					<View style={styles.userContainer} key={index}>
+					  <Image
+						source={{
+						  uri: "https://firebasestorage.googleapis.com/v0/b/squadzoneapp.appspot.com/o/defaultPP.png?alt=media&token=7f90b50b-6321-484a-9d14-295fbfcfc32f",
+						}}
+						style={styles.userImageL}
+					  />
+					  <View style={styles.userSubContainer}>
+						<View style={styles.usernameContainerL}>
+						  <Text style={styles.usernameText}>{player.name}</Text>
+						</View>
+					  </View>
+					</View>
+				  ))}
+				</View>
+				<View style={styles.playerList && { alignItems: "flex-end" }}>
+				  <Text
+					style={{
+					  fontFamily: "SF-Pro-Semibold",
+					  fontSize: 14,
+					  paddingBottom: 10,
+					  alignSelf: "center",
+					}}
+				  >
+					{partidoData.team2.name}
+				  </Text>
+				  {partidoData.team2.players.map((player, index) => (
+					<View style={styles.userContainer} key={index}>
+					  <View style={styles.userSubContainer}>
+						<View style={styles.usernameContainerR}>
+						  <Text style={styles.usernameText}>{player.name}</Text>
+						</View>
+					  </View>
+					  <Image
+						source={{
+						  uri: "https://firebasestorage.googleapis.com/v0/b/squadzoneapp.appspot.com/o/defaultPP.png?alt=media&token=7f90b50b-6321-484a-9d14-295fbfcfc32f",
+						}}
+						style={styles.userImageR}
+					  />
+					</View>
+				  ))}
+				</View>
+			  </View>
 			</View>
-
+	  
 			<View style={styles.buttonContainer}>
-				<TouchableOpacity style={styles.startGameButton} onPress={tempButton}>
-					<Text style={styles.startGameText}>Empezar Partido</Text>
-				</TouchableOpacity>
-
-				<TouchableOpacity style={styles.menuButton} onPress={tempButton}>
-					<Ionic name="ellipsis-vertical" style={styles.menuIcon} />
-				</TouchableOpacity>
+			  <TouchableOpacity style={styles.startGameButton} onPress={tempButton}>
+				<Text style={styles.startGameText}>Empezar Partido</Text>
+			  </TouchableOpacity>
+	  
+			  <TouchableOpacity style={styles.menuButton} onPress={tempButton}>
+				<Ionic name="ellipsis-vertical" style={styles.menuIcon} />
+			  </TouchableOpacity>
 			</View>
-		</BackgroundTabs>
-	);
+		  </BackgroundTabs>
+		) : (
+		  <Text>Loading...</Text>
+		)
+	  );
+	  
 };
 
 const styles = StyleSheet.create({
